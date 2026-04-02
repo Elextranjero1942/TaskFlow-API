daa44c0bf216d79f3eda86a4b23a0aa26caa8217
Elextranjero1942 <luismitmap@outlook.com>
2026-04-01 23:36:42 -0500

    Se agrego auth con argon2 y jwt

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from app.routers import auth
+from app.models import User, Task
 
 app = FastAPI(title="TaskFlow API")
 
+app.include_router(auth.router)
+
 @app.get("/")
 def root():
-    return {"message": "TaskFlow API funcionando"}
+    return {"message": "TaskFlow api funcionando"}
